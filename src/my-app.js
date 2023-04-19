ef4b2c1055524b0be05467de5d6399b464618955
Guardian <102593555+SafeGuardian@users.noreply.github.com>
2023-04-19 16:17:54 +0200

    Update my-app.js

--- src/my-app.js
+++ src/my-app.js
@@ -129,10 +129,10 @@ class MyApp extends PolymerElement {
           box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
         }
         app-toolbar {
-          background-color: #225277;
+          background-color: #fff;
         }
         paper-tabs {
-          background-color: #fff;
+          background-color: #225277;
         }
         marquee {
           background-color: #042138;
